a62295208e6fd71f8585e71d079ffe1c88a04d70
Thiago Lemos <thiago.lemos.ps@gmail.com>
2025-05-10 11:05:41 -0300

    Primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="Banner"> 
+            <img src="/imagens/banner.png" alt="O banner principal da pagina da Organo" />
+        </header>
+    )
+}
